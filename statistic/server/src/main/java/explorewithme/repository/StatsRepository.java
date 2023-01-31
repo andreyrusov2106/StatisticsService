e1b42b2b9b39332f43589865db0fb651e06af622
andrey.rusov <rusov2106@mail.ru>
2023-01-31 12:51:40 +0300

    add native queries

--- statistic/server/src/main/java/explorewithme/repository/StatsRepository.java
+++ statistic/server/src/main/java/explorewithme/repository/StatsRepository.java
@@ -9,14 +9,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsRepository extends PagingAndSortingRepository<Stats, Long> {
-    @Query(value = "Select sel1.uri, sel1.app, count(*) From (SELECT Distinct(s1.ip), s1.uri,s1.app " +
-            "FROM  statistic s1 where s1.uri in(:uri) and s1.created between :start and :end) sel1 \n" +
-            "GROUP BY sel1.uri, sel1.app", nativeQuery = true)
+    @Query(value = "With cte_statistic as(\n" +
+            "    SELECT Distinct(s1.ip), s1.uri,s1.app\n" +
+            "    FROM  statistic s1 where s1.uri in(:uri) and s1.created between :start and :end)\n" +
+            "Select uri, app, count(*) From cte_statistic\n" +
+            "GROUP BY uri, app", nativeQuery = true)
     List<StatResultProjection> findDistinctByIpAndTimeStampBetweenAndUriInNative(LocalDateTime start, LocalDateTime end, List<String> uri);
 
-    @Query(value = "Select sel1.uri, sel1.app, count(*) From (SELECT Distinct(s1.ip), s1.uri,s1.app " +
-            "FROM  statistic s1 where s1.created between :start and :end) sel1 \n" +
-            "GROUP BY sel1.uri, sel1.app", nativeQuery = true)
+    @Query(value = "With cte_statistic as(\n" +
+            "    SELECT Distinct(s1.ip), s1.uri,s1.app\n" +
+            "    FROM  statistic s1 where s1.created between :start and :end)\n" +
+            "Select uri, app, count(*) From cte_statistic\n" +
+            "GROUP BY uri, app", nativeQuery = true)
     List<StatResultProjection> findDistinctByIpAndTimeStampBetweenAndNative(LocalDateTime start, LocalDateTime end);
 
     @Query(value = "Select uri, app, count(*) " +
